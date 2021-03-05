8ae0821830004537d196ea6a2acefd4142be92c2
Arkaprabha Chakraborty <arkaprabha.chakraborty.ac@gmail.com>
2021-03-05 14:00:23 +0530

    Added DaysBetweenDates.java

---
+++ src/DaysBetweenDates.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.time.*;
+import java.time.temporal.*;
+
+public class DaysBetweenDates {
+
+
+    // Declaring vaiables
+    int[] DIAM;
+    String date_1, date_2;
+    long days;
+
+    // Constructor for initializing variables
+    DaysBetweenDates() {
+        DIAM = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        date_1 = "";
+        date_2 = "";
+        days = 0;
+    }
+    
+    // Main method for running the program
+    public static void main(String[] args) throws IOException {
+        DaysBetweenDates nod = new DaysBetweenDates();
+        nod.input();
+        nod.compute();
+        nod.output();
+    }
+
+    // Input method for taking inputs from the user
+    void input() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter starting date in YYYY-MM-DD format: ");
+        date_1 = in.readLine();
+        System.out.print("Enter ending date in YYYY-MM-DD format: ");
+        date_2 = in.readLine();
+    }
+
+    // Compute method for calculating days between the dates
+    void compute() {
+	    LocalDate ld_1 = LocalDate.parse(date_1);
+	    LocalDate ld_2 = LocalDate.parse(date_2);
+		
+	    days = ChronoUnit.DAYS.between(ld_1, ld_2);
+    }
+
+    // Output method for displaying the results
+    void output() {
+        System.out.println("Days between starting and ending date: " + days);
+    }
+}
